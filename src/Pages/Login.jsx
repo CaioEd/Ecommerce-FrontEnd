9d38affec95637d941a2d6a864e2e4de6fddbf84
Caio Eduardo Ribeiro Bento <caioersb@gmail.com>
2024-10-30 10:11:13 -0300

    Login de usuário

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,42 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import './Login.css'
 
 import Header from '../components/Header'
-import Products from '../components/Products'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const navigate = useNavigate()
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch('http://127.0.0.1:8000/login/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      })
+      const data = await response.json()
+
+      if (!response.ok) {
+        throw new Error(data.error || 'Erro ao fazer login')
+      }
+
+      // SAVE IN LocalStorage
+      localStorage.setItem('access_token', data.access)
+      localStorage.setItem('refresh_token', data.refresh)
+
+      navigate('/') // Redirect to Home Page
+    } catch (err) {
+      setError(err.message)
+    }
+  }
+
   return (
     <>
         <Header/>
@@ -13,11 +44,22 @@ const Login = () => {
         <div className="login">
             <h1>Inicie sessão para realizar compras</h1>
             <h2>Iniciar a sessão</h2>
-            <form action="">
-                <input type="email" name="email" id="email" required placeholder='E-mail' />
-                <input type="password" name="password" id="password" placeholder='Senha' />
+            <form onSubmit={handleLogin}>
+                <input type="txt" name="username" id="username" 
+                required placeholder='Nome de usuário' 
+                onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <input type="password" name="password" id="password" 
+                required placeholder='Senha' 
+                onChange={(e) => setPassword(e.target.value)}
+                />
+
                 <button className='login-button'>Iniciar sessão</button>
             </form>
+            
+           
+            {error && <p>{error}</p>} {/* Exibir mensagem de erro */}
 
             <span>
                 <Link to="/registration">Não possui conta? Crie já.</Link>
--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -6,40 +6,25 @@ const UserPage = () => {
   return (
     <>
         <Header/>
-
+        <h2>Editar dados de usuário:</h2>
         <div className="form-container" >
             <form action="#" method="POST">
                 <div className="form-group">
-                <label htmlFor="nome">Nome:</label>
+                <label htmlFor="nome">Nome de usuário:</label>
                 <input type="text" id="nome" name="nome" required />
-                </div>
-                
-                <div className="form-group">
-                <label htmlFor="dataNascimento">Data de Nascimento:</label>
-                <input type="date" id="dataNascimento" name="dataNascimento" required />
                 </div>
                 
                 <div className="form-group">
                 <label htmlFor="email">Email:</label>
                 <input type="email" id="email" name="email" required />
                 </div>
-                
+
                 <div className="form-group">
-                <label htmlFor="celular">Número de Celular:</label>
-                <input type="tel" id="celular" name="celular" pattern="[0-9]{11}" placeholder="Ex: 11999999999" required />
-                </div>
-                
-                <div className="form-group">
-                <label htmlFor="cep">CEP:</label>
-                <input type="text" id="cep" name="cep" pattern="[0-9]{8}" placeholder="Ex: 12345678" required />
-                </div>
-                
-                <div className="form-group">
-                <label htmlFor="numero">Número:</label>
-                <input type="text" id="numero" name="numero" required />
+                <label htmlFor="password">Senha:</label>
+                <input type="password" id="email" name="password" required />
                 </div>
 
-                <button className='save_info' type="submit">Salvar</button>
+                <button className='save_info' type="submit">Editar</button>
             </form>
         </div>
 
